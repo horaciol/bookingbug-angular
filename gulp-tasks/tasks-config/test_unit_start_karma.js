42f6d9a8e2bb0f03af8a99d5d86d3ed43196043a
damian pudlo <pudlo.damian@gmail.com>
2016-10-21 15:26:18 +0100

    unit-tests fix

--- gulp-tasks/tasks-config/test_unit_start_karma.js
+++ gulp-tasks/tasks-config/test_unit_start_karma.js
@@ -17,10 +17,9 @@
                 }
             });
             projectFiles = [
-                'src/core/javascripts/main.js.coffee',
+                'src/*/javascripts/**/*.module.js.coffee',
                 'src/*/javascripts/main.js.coffee',
                 'src/*/javascripts/core/config.js.coffee',
-                'src/*/javascripts/**/*.module.js.coffee',
                 'src/*/templates/**/*.html',
                 'src/*/templates/*.html',
                 'src/core/javascripts/collections/*.coffee',
